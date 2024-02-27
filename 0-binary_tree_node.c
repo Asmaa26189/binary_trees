9fe7e596780b65f8db7306c7cdf626193d5c0162
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-27 11:20:37 +0200

    27-2-2024

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,12 +16,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	if (n_node != NULL)
-	{
 	n_node->n = value;
 	n_node->parent = parent;
 	n_node->left = NULL;
 	n_node->right = NULL;
-    }
-    return (n_node);
+	return (n_node);
 }
