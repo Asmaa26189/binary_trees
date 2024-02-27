82d261c47b7066eb07ecdda6c5fd153279ac6285
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-27 11:25:06 +0200

    27-2-2024

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,6 +6,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t value = ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
-    return (value);
+    return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
